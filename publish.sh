ad81c22d1c3a55c2290b66b0edf175834a1243dc
Naveen Tirumalasetty <tsnaveen@hotmail.com>
2017-11-21 17:43:01 -0600

    lets try publish

--- publish.sh
+++ publish.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-#
+
 COMPONENT="aspnetcore-generator-api"
 VERSION=1.0
 BUILD_NUMBER=1
-#IMAGE1="tsnaveen/${COMPONENT}:${VERSION}-${BUILD_NUMBER-0}"
+IMAGE1="tsnaveen/${COMPONENT}:${VERSION}-${BUILD_NUMBER-0}"
 IMAGE2="tsnaveen/${COMPONENT}:latest"
-#TAG="v${VERSION}-${BUILD_NUMBER-0}"
-#
+TAG="v${VERSION}-${BUILD_NUMBER-0}"
+
 #cd /cygdrive/c/Repositories/learning/docker/aspnotecore-generator-api
-#
+
 # Any subsequent(*) commands which fail will cause the shell scrupt to exit immediately
 set -e
 set -o pipefail
 #
 # Set tag on git repo
-#git tag $TAG
-#git push --tags
+git tag $TAG
+git push --tags
 #
 export $DOCKER_USER=tsnaveen
 export $DOCKER_PASS=Shanmukh10
@@ -24,4 +24,3 @@ export $DOCKER_PASS=Shanmukh10
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 docker push $IMAGE1
 docker push $IMAGE2
-
